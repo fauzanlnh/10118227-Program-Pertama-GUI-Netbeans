5825e89600a16d850d5a04906de1d374cff279bf
fauzanlnh <fauzanlh2000@gmail.com>
2020-03-26 14:53:26 +0700

    First Upload

---
+++ src/MainClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fauzanlh
+ */
+public class MainClass {
+    
+    public static void main(String[] args) {
+        Frame1 F1 = new Frame1();
+        F1.setTittle("Program Pertama GUI - NETBEANS");
+    }
+}
